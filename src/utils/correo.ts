81b992735fd34f5d93e84384dbe955279a50cf79
T0masTapia <132634784+T0masTapia@users.noreply.github.com>
2025-09-26 12:09:48 -0300

    Correos Funcionando

--- src/utils/correo.ts
+++ src/utils/correo.ts
@@ -4,17 +4,29 @@ dotenv.config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
-export const enviarCorreo = async (para: string, asunto: string, mensaje: string, p0: { filename: string; content: Buffer<ArrayBufferLike>; contentType: string; }[]) => {
+export const enviarCorreo = async (
+  para: string,
+  asunto: string,
+  mensaje: string,
+  adjuntos?: { filename: string; content: Buffer; contentType: string }[]
+) => {
   try {
     const msg = {
       to: para,
       from: process.env.FROM_EMAIL!,
       subject: asunto,
       html: mensaje,
+      attachments: adjuntos?.map(file => ({
+        content: file.content.toString('base64'),
+        filename: file.filename,
+        type: file.contentType,
+        disposition: 'attachment',
+      })),
     };
+
     await sgMail.send(msg);
     console.log('✅ Correo enviado a:', para);
-  } catch (error) {
-    console.error('Error enviando correo:', error);
+  } catch (error: any) {
+    console.error('Error enviando correo:', error.response?.body || error);
   }
 };
